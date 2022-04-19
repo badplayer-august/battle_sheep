3926701506bcb815d684349c677d64d3296606c3
jasonsie88 <56729568+jasonsie88@users.noreply.github.com>
2022-04-19 20:18:30 +0800

    Add files via upload

--- pacman_Sample_v2/python/Sample.py
+++ pacman_Sample_v2/python/Sample.py
@@ -63,12 +63,14 @@ def getStep(playerStat, ghostStat, propsStat,otherPlayerStat,right_map,left_map,
         reward_map[x][y]=-100 # ghost, negative reward   
     landmine = False
     #move = random.choice([0, 1, 2, 3, 4])
-    dirs = [[-1,0],[1,0],[0,1],[0,-1]] # move directions, left,right,up,down
+    dirs = [[0,-1],[0,1],[1,0],[-1,0]] # move directions, left,right,up,down
     x = int(playerStat[0])
     y = int(playerStat[1])
-    limit_x = max(int(playerStat[0]/25)+12,16)# define search space
-    limit_y = max(int(playerStat[1]/25)+12,16)
-    best_score = float("-inf")
+    limit_max_x = max(int(playerStat[0]/25)+8,16)# define search space
+    limit_min_x = max(int(playerStat[0]/25)-8,0)
+    limit_max_y = max(int(playerStat[1]/25)+8,16)
+    limit_min_y = max(int(playerStat[1]/25)-8,0)
+    best_score = 0
     move=0
     global enemy 
     enemy = False # check if ghosts or players are aeound
@@ -79,10 +81,10 @@ def getStep(playerStat, ghostStat, propsStat,otherPlayerStat,right_map,left_map,
         visit[int(playerStat[0]/25)][int(playerStat[1]/25)]=0
         x = int(playerStat[0]/25)+dir[0]
         y = int(playerStat[1]/25)+dir[1]
-        search(x,y,limit_x,limit_y,visit,score,reward_map,left_map,right_map,up_map,down_map)
+        search(x,y,limit_max_x,limit_max_y,limit_min_x,limit_min_y,visit,score,reward_map,left_map,right_map,up_map,down_map)
         result = sum(score)
         #print(result)
-        if result > best_score:
+        if result >= best_score:
             best_move = move
             best_score = result
         move = move+1
@@ -93,28 +95,27 @@ def getStep(playerStat, ghostStat, propsStat,otherPlayerStat,right_map,left_map,
     #    print("here")
     if playerStat[2] > 0 and enemy==True:
         landmine = True
+    if best_move==4:
+        best_move = random.choice([0, 1, 2, 3])
     action = [best_move, landmine]
-def search(x,y,limit_x,limit_y,visit,score,reward_map,left_map,right_map,up_map,down_map): # DFS
+    
+def search(x,y,limit_max_x,limit_max_y,limit_min_x,limit_min_y,visit,score,reward_map,left_map,right_map,up_map,down_map): # DFS
+    if x>=16 or x<0 or y>=16 or y<0 or visit[x][y]==0:
+        return
     visit[x][y]=0
     score.append(reward_map[x][y])
     if reward_map[x][y]==-100:
         enemy=True
-    if x+1<16 and x+1>=0 and y<16 and y>=0 and x+1<=limit_x and y<=limit_y and visit[x+1][y]==1 and down_map[x+1][y]==1:
-        search(x+1,y,limit_x,limit_y,visit,score,reward_map,left_map,right_map,up_map,down_map)
-    else:
-        return
-    if x-1<16 and x-1>=0 and y<16 and y>=0 and x-1<=limit_x and y<=limit_y and visit[x-1][y]==1 and up_map[x-1][y]==1:
-        search(x-1,y,limit_x,limit_y,visit,score,reward_map,left_map,right_map,up_map,down_map)
-    else:
-        return
-    if x<16 and x>=0 and y+1<16 and y+1>=0 and x<=limit_x and y+1<=limit_y and visit[x][y+1]==1 and right_map[x][y+1]==1:
-        search(y+1,y,limit_x,limit_y,visit,score,reward_map,left_map,right_map,up_map,down_map)
-    else:
-        return
-    if x<16 and x>=0 and y-1<16 and y-1>=0 and x<=limit_x and y-1<=limit_y and visit[x][y-1]==1 and left_map[x][y-1]==1:
-        search(y-1,y,limit_x,limit_y,visit,score,reward_map,left_map,right_map,up_map,down_map)
-    else:
-        return
+    if  x+1<=limit_max_x and down_map[x][y]==1:
+        search(x+1,y,limit_max_x,limit_max_y,limit_min_x,limit_min_y,visit,score,reward_map,left_map,right_map,up_map,down_map)
+    if  x-1>=limit_min_x and up_map[x][y]==1:
+        search(x-1,y,limit_max_x,limit_max_y,limit_min_x,limit_min_y,visit,score,reward_map,left_map,right_map,up_map,down_map)
+
+    if  y+1<=limit_max_y and  right_map[x][y]==1:
+        search(y+1,y,limit_max_x,limit_max_y,limit_min_x,limit_min_y,visit,score,reward_map,left_map,right_map,up_map,down_map)
+
+    if  y-1>=limit_min_y and left_map[x][y]==1:
+        search(y-1,y,limit_max_x,limit_max_y,limit_min_x,limit_min_y,visit,score,reward_map,left_map,right_map,up_map,down_map)
 # props img size => pellet = 5*5, landmine = 11*11, bomb = 11*11
 # player, ghost img size=23x23
 
@@ -174,6 +175,7 @@ if __name__ == "__main__":
         
         user_thread = MyThread(target=getStep, args=(playerStat, ghostStat, propsStat,otherPlayerStat,right_map,left_map,up_map,down_map))
         user_thread.start()
+        print(action)
         time.sleep(4/100)
         #print(action)
         if action == None:
