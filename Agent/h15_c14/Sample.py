7917239ec97f398e1a393a68b28a59da93c7b5a9
badplayer-august <76912501+badplayer-august@users.noreply.github.com>
2022-04-24 14:19:20 +0800

    add agent 1514

--- Agent/h15_c14/Sample.py
+++ Agent/h15_c14/Sample.py
@@ -6,17 +6,19 @@ import random
 '''
     選擇起始位置
     選擇範圍僅限場地邊緣(至少一個方向為牆)
-    
+
     return: init_pos
     init_pos=[x,y],代表起始位置
-    
+
 '''
 
+c_param = 1.4
+h = 15
 
 def InitPos(mapStat):
     mapStat = np.array(mapStat, dtype='int32').T
     print("MapState",mapStat)
-    
+
     sheepStat = np.zeros((12, 12),dtype='int32')
     sheepStat[mapStat>0] = 16
     sheepStat = list(sheepStat)
@@ -24,23 +26,23 @@ def InitPos(mapStat):
     playerID = 1
     while (mapStat == playerID).any():
         playerID += 1
-    
-    action = MCTS(MCTS_NODE(BattleSheepState(mapStat, sheepStat, playerID))).best_action(c_param=1.4, h=15)
+
+    action = MCTS(MCTS_NODE(BattleSheepState(mapStat, sheepStat, playerID))).best_action(c_param=c_param, h=h)
     return [action.j, action.i]
 
 
 '''
     產出指令
-    
-    input: 
+
+    input:
     playerID: 你在此局遊戲中的角色(1~4)
-    mapStat : 棋盤狀態(list of list), 為 12*12矩陣, 
+    mapStat : 棋盤狀態(list of list), 為 12*12矩陣,
               0=可移動區域, -1=障礙, 1~4為玩家1~4佔領區域
     sheepStat : 羊群分布狀態, 範圍在0~16, 為 12*12矩陣
 
     return Step
     Step : 3 elements, [(x,y), m, dir]
-            x, y 表示要進行動作的座標 
+            x, y 表示要進行動作的座標
             m = 要切割成第二群的羊群數量
             dir = 移動方向(1~6),對應方向如下圖所示
               1  2
@@ -48,7 +50,7 @@ def InitPos(mapStat):
               5  6
 '''
 def GetStep(playerID, mapStat, sheepStat):
-    action = MCTS(MCTS_NODE(BattleSheepState(np.array(mapStat, dtype='int32').T, list(np.array(sheepStat, dtype='int32').T), playerID))).best_action(1000, c_param=1.4, h=10)
+    action = MCTS(MCTS_NODE(BattleSheepState(np.array(mapStat, dtype='int32').T, list(np.array(sheepStat, dtype='int32').T), playerID))).best_action(c_param=c_param, h=h)
     return [(action.j, action.i), action.m, action.d + 1]
 
 
