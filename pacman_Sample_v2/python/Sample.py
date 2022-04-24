6a34abd531631130dfba413d2697d4e94e0e9cb7
jasonsie88 <56729568+jasonsie88@users.noreply.github.com>
2022-04-25 03:35:25 +0800

    Add files via upload

--- pacman_Sample_v2/python/Sample.py
+++ pacman_Sample_v2/python/Sample.py
@@ -39,38 +39,57 @@ def getStep(playerStat, ghostStat, propsStat,otherPlayerStat,right_map,left_map,
     global pre_x
     global pre_y
     bomb = False
-    if playerStat[2] > 0:
-        bomb = True
+    
+    #print("super time:", playerStat[3])
+    if playerStat[3] > 0:
+        super_time=10
+    else:
+        super_time=-1
     '''
     control of your player
     0: left, 1:right, 2: up, 3: down 4:no control
     format is (control, set landmine or not) = (0~3, True or False)
     put your control in action and time limit is 0.04sec for one step
     '''
-    dirs = [[0,-1],[0,1],[-1,0],[1,0]]
     reward_map=np.zeros((16,16),dtype='int32')
     maze_wall=[left_map,right_map,up_map,down_map]
     for prop in propsStat:
         x = int(prop[1]/25)
         y = int(prop[2]/25)
         if prop[0]==3:
-            reward_map[x][y]=-100 #active landmines,negative reward
+            reward_map[x][y]+=-100 #active landmines,negative reward
+            for i in range(16):
+                reward_map[i][y]+=-50*(16-abs(x-i))
+                reward_map[x][i]+=-50*(16-abs(y-i))
+            
         elif prop[0]==0:
-            reward_map[x][y]=100 #power pellets, positive reward
+            reward_map[x][y]+=10000 #power pellets, positive reward
+            for i in range(16):
+                reward_map[i][y]+=5000*(16-abs(x-i))
+                reward_map[x][i]+=5000*(16-abs(y-i))            
         elif prop[0]==1:
-            reward_map[x][y]=100 # landmines, positive reward
+            reward_map[x][y]+=100 # landmines, positive reward
+            for i in range(16):
+                reward_map[i][y]+=50*(16-abs(x-i))
+                reward_map[x][i]+=50*(16-abs(y-i))          
         elif prop[0]==2:
-            reward_map[x][y]=50 #pellets, reward
+            reward_map[x][y]+=500 #pellets, reward
+            for i in range(16):
+                reward_map[i][y]+=300*(16-abs(x-i))
+                reward_map[x][i]+=300*(16-abs(y-i))             
     for other in otherPlayerStat:
         x = int(other[0]/25)
         y = int(other[1]/25)
         if other[2] > 0:
-            reward_map[x][y]=-100 #other players have landmines, negative reward
+            reward_map[x][y]+=-100 #other players have landmines, negative reward
     for ghost in ghostStat:
         x = int(ghost[0]/25)
         y = int(ghost[1]/25)
-        reward_map[x][y]=-100 # ghost, negative reward   
-    best_reward = -1000
+        reward_map[x][y]+=super_time*100 # ghost, negative reward
+        for i in range(16):
+            reward_map[i][y]+=super_time*100*(16-abs(x-i))
+            reward_map[x][i]+=super_time*100*(16-abs(y-i))                
+    best_reward = float("-inf")
     best_move=4
     x = int(playerStat[0]/25)
     y = int(playerStat[1]/25)
@@ -82,6 +101,7 @@ def getStep(playerStat, ghostStat, propsStat,otherPlayerStat,right_map,left_map,
     #print("x: ",x)
     #print("y: ",y)
     #print("pre move:",pre_move)
+    way = [[-1,0],[1,0],[0,-1],[0,1]]
     if pre_move !=-100:
         dir=[]
         if pre_move%2==1:
@@ -93,23 +113,27 @@ def getStep(playerStat, ghostStat, propsStat,otherPlayerStat,right_map,left_map,
                 dir.append(i)
         for d in dir:
              wall = maze_wall[d]
-             
              if wall[x][y]==1:
-                reward = copy.deepcopy(reward_map[x][y])
-                #print("d: ",d)
-                #print("maze: ",wall[x][y])
-                if reward >best_reward:
-                    best_move=copy.deepcopy(d)
-                    best_reward=copy.deepcopy(reward)           
+                if x+way[d][0]>=0 and y+way[d][1]>=0 and x+way[d][0]<=15 and y+way[d][1]<=15:
+                    reward = copy.deepcopy(reward_map[x+way[d][0]][y+way[d][1]])
+                    if reward <500 and playerStat[2] > 0:
+                        bomb=True
+                    #print("d: ",d)
+                    #print("maze: ",wall[x][y])
+                    if reward >=best_reward:
+                        best_move=copy.deepcopy(d)
+                        #best_move_choice.append(d)
+                        best_reward=copy.deepcopy(reward)       
     else:
         for i in range(4):
             wall = maze_wall[i]
             if wall[x][y]==1:
-                reward = copy.deepcopy(reward_map[x][y])
-                #print(wall[x][y])
-                if reward >best_reward:
-                    best_move=copy.deepcopy(i)
-                    best_reward=copy.deepcopy(reward)
+                if x+way[i][0]>=0 and y+way[i][1]>=0 and x+way[i][0]<=15 and y+way[i][1]<=15:
+                    reward = copy.deepcopy(reward_map[x+way[i][0]][y+way[i][0]])
+                    #print(wall[x][y])
+                    if reward >=best_reward:
+                        best_move=copy.deepcopy(i)
+                        best_reward=copy.deepcopy(reward)
     pre_move = best_move
     action =[best_move,bomb]    
     pre_x = int(playerStat[0]/25)
@@ -122,23 +146,6 @@ def getStep(playerStat, ghostStat, propsStat,otherPlayerStat,right_map,left_map,
     #    landmine = True
     #action = [best_move, landmine]
     
-def search(x,y,limit_max_x,limit_max_y,limit_min_x,limit_min_y,visit,score,reward_map,left_map,right_map,up_map,down_map): # DFS
-    if x>=16 or x<0 or y>=16 or y<0 or visit[x][y]==0:
-        return
-    visit[x][y]=0
-    score.append(reward_map[x][y])
-    if reward_map[x][y]==-100:
-        enemy=True
-    if  x+1<=limit_max_x and down_map[x][y]==1:
-        search(x+1,y,limit_max_x,limit_max_y,limit_min_x,limit_min_y,visit,score,reward_map,left_map,right_map,up_map,down_map)
-    if  x-1>=limit_min_x and up_map[x][y]==1:
-        search(x-1,y,limit_max_x,limit_max_y,limit_min_x,limit_min_y,visit,score,reward_map,left_map,right_map,up_map,down_map)
-
-    if  y+1<=limit_max_y and  right_map[x][y]==1:
-        search(y+1,y,limit_max_x,limit_max_y,limit_min_x,limit_min_y,visit,score,reward_map,left_map,right_map,up_map,down_map)
-
-    if  y-1>=limit_min_y and left_map[x][y]==1:
-        search(y-1,y,limit_max_x,limit_max_y,limit_min_x,limit_min_y,visit,score,reward_map,left_map,right_map,up_map,down_map)
 # props img size => pellet = 5*5, landmine = 11*11, bomb = 11*11
 # player, ghost img size=23x23
 
